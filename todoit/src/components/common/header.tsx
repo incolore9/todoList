691be4db00dd19dc385fcefe1feb5f78494dbc2f
incolore9 <hansol.woff@gmail.com>
2025-01-20 23:25:36 +0900

    [refactor] 헤더 이미지 태그 수정, 필요 깃 ignore 추가

--- todoit/src/components/common/header.tsx
+++ todoit/src/components/common/header.tsx
@@ -1,11 +1,17 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Header() {
   return (
     <nav className="h-[60px] w-full border-b-[1px] bg-white">
       <div className="m-auto flex h-full max-w-[1200px] items-center">
         <Link href="/">
-          <img src="/image/common/Size=Large.svg" alt="do it logotype " />
+          <Image
+            src="/image/common/Size=Large.svg"
+            alt="do it logotype"
+            width={150}
+            height={100}
+          />
         </Link>
       </div>
     </nav>
