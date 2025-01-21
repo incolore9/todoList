5b3d8ffaef1c83bff0a26f25fba9b018e61f981b
incolore9 <hansol.woff@gmail.com>
2025-01-21 19:29:13 +0900

    [style] 전역 그림자 div 스타일 추가

--- todoit/src/app/page.tsx
+++ todoit/src/app/page.tsx
@@ -4,9 +4,17 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="m-auto h-screen max-w-[1200px] p-4">
+      <div className="mb-[21px]">
+        <input
+          type="text"
+          placeholder="할 일을 입력해주세요"
+          className="shadowDiv w-full"
+        />
+      </div>
+
       <div className="flex gap-6">
         <div className="w-1/2">
-          <div className="mb-4 w-[101px] h-9 rounded-full bg-lime-300 px-4 py-2 flex items-center justify-center">
+          <div className="mb-4 flex h-9 w-[101px] items-center justify-center rounded-full bg-lime-300 px-4 py-2">
             <Image
               src="/image/common/TODO-logo.svg"
               width={50}
@@ -18,7 +26,7 @@ export default function Home() {
           <TodoList />
         </div>
         <div className="w-1/2">
-          <div className="mb-4 w-[101px] h-9 rounded-full bg-green-700 px-4 py-2 flex items-center justify-center">
+          <div className="mb-4 flex h-9 w-[101px] items-center justify-center rounded-full bg-green-700 px-4 py-2">
             <Image
               src="/image/common/DONE-logo.svg"
               width={50}
