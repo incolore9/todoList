345c009224250ecdeba3229a3b0c52d779c9e686
incolore9 <hansol.woff@gmail.com>
2025-01-24 09:17:25 +0900

    [feat] 항목 비었을 때 배경 변경 가능

--- todoit/src/app/page.tsx
+++ todoit/src/app/page.tsx
@@ -70,7 +70,9 @@ export default function Home() {
       </div>
 
       <div className="flex flex-col gap-6 md:flex-row">
-        <div className="w-full md:w-1/2">
+        <div
+          className={`w-full md:w-1/2 ${todos.filter((todo) => !todo.isCompleted).length === 0 ? "bg-yellow-500" : ""}`}
+        >
           <div className="mb-4 flex h-9 w-[101px] items-center justify-center rounded-full bg-lime-300 px-4 py-2">
             <Image
               src="/image/common/TODO-logo.svg"
@@ -86,7 +88,9 @@ export default function Home() {
             ))}
         </div>
 
-        <div className="w-full md:w-1/2">
+        <div
+          className={`w-full md:w-1/2 ${todos.filter((todo) => todo.isCompleted).length === 0 ? "bg-red-500" : ""}`}
+        >
           <div className="mb-4 flex h-9 w-[101px] items-center justify-center rounded-full bg-green-700 px-4 py-2">
             <Image
               src="/image/common/DONE-logo.svg"
