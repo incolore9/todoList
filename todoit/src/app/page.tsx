11456a801b78d06522159049f8c4c4d0a7f48b81
incolore9 <hansol.woff@gmail.com>
2025-01-25 16:22:21 +0900

    [refactor] 첫 홈화면을 서버 컴포넌트로 전환 후, 기능 부분을 분리

--- todoit/src/app/page.tsx
+++ todoit/src/app/page.tsx
@@ -1,55 +1,16 @@
-"use client"; // 임시
-import { useState, useEffect } from "react";
+import HomeClient from "@/components/home/TodoContainer";
 
-import { addTodo } from "@/app/utils/todoActions";
-import TodoForm from "@/components/TodoForm";
-import TodoSection from "@/components/TodoSection";
+async function getTodos() {
+  const API_URL = `${process.env.NEXT_PUBLIC_API_URL}/${process.env.NEXT_PUBLIC_TENANT_ID}/items`;
+  const res = await fetch(API_URL);
+  if (!res.ok) {
+    throw new Error("Todo items를 불러오는데 실패했습니다.");
+  }
+  return res.json();
+}
 
-const API_URL = `${process.env.NEXT_PUBLIC_API_URL}/${process.env.NEXT_PUBLIC_TENANT_ID}/items`;
+export default async function Home() {
+  const todos = await getTodos();
 
-export default function Home() {
-  const [todos, setTodos] = useState([]);
-
-  useEffect(() => {
-    fetch(API_URL)
-      .then((res) => res.json())
-      .then((data) => setTodos(data));
-  }, []);
-
-  const handleAddTodo = async (formData) => {
-    const createTodo = await addTodo(formData);
-    setTodos((previousTodos) => [...previousTodos, createTodo]);
-  };
-
-  const toggleTodo = async (id) => {
-    const updatedTodos = todos.map((check) =>
-      check.id === id ? { ...check, isCompleted: !check.isCompleted } : check,
-    );
-    setTodos(updatedTodos);
-
-    await fetch(`${API_URL}/${id}`, {
-      method: "PATCH",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        isCompleted: !todos.find((todo) => todo.id === id).isCompleted,
-      }),
-    });
-  };
-
-  return (
-    <div className="m-auto h-screen max-w-[1200px] p-4">
-      <div className="mb-10 flex w-full gap-[26px] max-md:gap-4 max-sm:mb-6">
-        <TodoForm onAddTodo={handleAddTodo} />
-      </div>
-
-      <div className="flex flex-col gap-6 md:flex-row">
-        <TodoSection
-          todos={todos}
-          toggleTodo={toggleTodo}
-          isCompleted={false}
-        />
-        <TodoSection todos={todos} toggleTodo={toggleTodo} isCompleted={true} />
-      </div>
-    </div>
-  );
+  return <HomeClient initialTodos={todos} />;
 }
---
+++ todoit/src/components/home/TodoContainer.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { useState } from "react";
+import { addTodo } from "@/app/utils/todoActions";
+import TodoForm from "@/components/TodoForm";
+import TodoSection from "@/components/TodoSection";
+
+const API_URL = `${process.env.NEXT_PUBLIC_API_URL}/${process.env.NEXT_PUBLIC_TENANT_ID}/items`;
+
+export default function HomeClient({ initialTodos }: { initialTodos: any[] }) {
+  const [todos, setTodos] = useState(initialTodos || []);
+
+  const handleAddTodo = async (formData: FormData) => {
+    const createTodo = await addTodo(formData);
+    setTodos((previousTodos) => [...previousTodos, createTodo]);
+  };
+
+  const toggleTodo = async (id: number) => {
+    const updatedTodos = todos.map((item) =>
+      item.id === id ? { ...item, isCompleted: !item.isCompleted } : item,
+    );
+    setTodos(updatedTodos);
+
+    await fetch(`${API_URL}/${id}`, {
+      method: "PATCH",
+      body: JSON.stringify({
+        isCompleted: !todos.find((todo) => todo.id === id)?.isCompleted,
+      }),
+    });
+  };
+
+  return (
+    <div className="m-auto h-screen max-w-[1200px] p-4">
+      <div className="mb-10 flex w-full gap-[26px] max-md:gap-4 max-sm:mb-6">
+        <TodoForm onAddTodo={handleAddTodo} />
+      </div>
+      <div className="flex flex-col gap-6 md:flex-row">
+        <TodoSection
+          todos={todos}
+          toggleTodo={toggleTodo}
+          isCompleted={false}
+        />
+        <TodoSection todos={todos} toggleTodo={toggleTodo} isCompleted={true} />
+      </div>
+    </div>
+  );
+}
